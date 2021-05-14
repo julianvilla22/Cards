e01113dba2f5a314fdb1fa329ee5b93c5d65b626
julianvilla22 <36893997+julianvilla22@users.noreply.github.com>
2021-05-14 16:46:28 +0200

    logout implementado

---
+++ app/src/main/java/es/uam/eps/dadm/cards/FirebaseSync.kt
@@ -0,0 +1,14 @@
+package es.uam.eps.dadm.cards
+
+import android.content.Context
+import es.uam.eps.dadm.cards.database.CardDatabase
+
+
+class FirebaseSync (
+    context: Context
+        )
+    {
+    fun uploadUserData(){
+
+    }
+}
--- app/src/main/java/es/uam/eps/dadm/cards/TitleFragment.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/TitleFragment.kt
@@ -1,6 +1,7 @@
 package es.uam.eps.dadm.cards
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import es.uam.eps.dadm.cards.databinding.FragmentTitleBinding
 
 class TitleFragment: Fragment() {
@@ -28,6 +32,13 @@ class TitleFragment: Fragment() {
         binding.cardsTitleTextView.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_titleFragment_to_deckListFragment)
             (activity as AppCompatActivity).supportActionBar?.show()
+        }
+        binding.logout.setOnClickListener {
+            var auth: FirebaseAuth
+            auth = Firebase.auth
+            auth.signOut()
+            val intent: Intent = Intent(it.context, EmailPasswordActivity::class.java)
+            startActivity(intent)
         }
 
         return binding.root
